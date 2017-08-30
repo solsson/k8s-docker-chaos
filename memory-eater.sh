43f576ec05953d254bf9297d22de3f8f9ee0fe99
Staffan Olsson <staffan@repos.se>
2017-08-30 16:15:32 +0200

    Use up to ~7GB, and stay there

--- memory-eater.sh
+++ memory-eater.sh
@@ -2,17 +2,12 @@
 
 #https://stackoverflow.com/questions/4964799/write-a-bash-shell-script-that-consumes-a-constant-amount-of-ram-for-a-user-defi
 
-echo "Provide sleep time in the form of NUMBER[SUFFIX]"
-echo "   SUFFIX may be 's' for seconds (default), 'm' for minutes,"
-echo "   'h' for hours, or 'd' for days."
-read -p "> " delay
-
 echo "begin allocating memory..."
-for index in $(seq 1000); do
+for index in $(seq 10000); do
     value=$(seq -w -s '' $index $(($index + 100000)))
     eval array$index=$value
 done
 echo "...end allocating memory"
 
-echo "sleeping for $delay"
-sleep $delay
+echo "staying here with the memory allocated"
+tail -f /dev/null
