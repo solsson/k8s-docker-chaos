e083552227836df16616f5ef4159f2efe02199a5
Staffan Olsson <staffan@repos.se>
2017-08-30 16:07:19 +0200

    Rather slow, but eats memory

---
+++ memory-eater.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#https://stackoverflow.com/questions/4964799/write-a-bash-shell-script-that-consumes-a-constant-amount-of-ram-for-a-user-defi
+
+echo "Provide sleep time in the form of NUMBER[SUFFIX]"
+echo "   SUFFIX may be 's' for seconds (default), 'm' for minutes,"
+echo "   'h' for hours, or 'd' for days."
+read -p "> " delay
+
+echo "begin allocating memory..."
+for index in $(seq 1000); do
+    value=$(seq -w -s '' $index $(($index + 100000)))
+    eval array$index=$value
+done
+echo "...end allocating memory"
+
+echo "sleeping for $delay"
+sleep $delay
